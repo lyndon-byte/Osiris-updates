e16ef69d3ed074e24dfd99d7e0d47f0db098678f
lyndon-byte <52107329+lyndon-byte@users.noreply.github.com>
2023-12-04 09:30:31 +0800

    last

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar(props){
                 <nav className="navbar navbar-expand-lg py-3 navbar-light bg-white border fixed-top">
                 <div className="container">
                     <a className="navbar-brand fs-5" href="/">
-                        <img src="/src/assets/osirislogo.png" width="70"  className="align-middle me-1" alt="My Website"/>
+                        <img src="./src/assets/osirislogo.png" width="70"  className="align-middle me-1" alt="My Website"/>
                         <h2 className="float-end mt-3" id='brandtext'>Osiris</h2>
                     </a>
 
@@ -28,11 +28,11 @@ function Navbar(props){
 
                     
                     <div className="lc-block ms-auto d-grid gap-2 d-lg-block">
-                        <a className={color} href="https://osiris-updates.vercel.app/login" role="button">
+                        <a className={color} href="/login" role="button">
                         <i className='fa-regular fa-user'></i>
                         &nbsp; Login
                         </a>
-                        <a className={color2} href="https://osiris-updates.vercel.app/register" role="button">Register</a>
+                        <a className={color2} href="/register" role="button">Register</a>
                     </div>
                     </div>
                 </div>
